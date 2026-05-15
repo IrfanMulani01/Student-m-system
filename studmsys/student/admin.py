e662134c1940e12df093aedb7551b9bd552d8a52
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-15 16:44:50 +0530

    create custom admin and register it and also registe student model

--- studmsys/student/admin.py
+++ studmsys/student/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class CustAdmin(admin.ModelAdmin):
+    list_display = ['fname', 'lname', 'username', 'password', 'gender', 'dateOfBirth', 'address', 'create_at', 'update_at']
+    ordering = ['id']
+    search_fields = ['fname']
+    list_filter= ['username']
+
+admin.site.register(Student, CustAdmin)
