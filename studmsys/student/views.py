b6662a31da7cf895b8261d7a0bb8aee88403cb68
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-15 16:45:32 +0530

    create main logic of login, register and list display

--- studmsys/student/views.py
+++ studmsys/student/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import *
+from .forms import LoginStudent,RegStudent
 
-# Create your views here.
+def registerStudent(request):
+    if request.method == 'POST':
+        form = RegStudent(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'temp/login.html')
+        
+    else:
+        form = RegStudent()
+
+    return render(request, 'temp/register.html', {'form': form})
+
+
+def loginStudent(request):
+    if request.method == 'POST':
+        form = LoginStudent(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'temp/index.html')
+        
+    else:
+        form = LoginStudent()
+
+    return render(request, 'temp/login.html', {'form':form})
+
+def studentList(request):
+    student = Student.objects.all()
+    return render(request, 'temp/studlist.html', {'stud':student})
