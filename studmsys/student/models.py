604e2579265cbf79acf4beb80da223a4aa096787
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-16 11:54:59 +0530

    set str method for display it's name on admin pannel

--- studmsys/student/models.py
+++ studmsys/student/models.py
@@ -24,3 +24,7 @@ class Student(models.Model):
     address = models.TextField()
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.fname
+    
