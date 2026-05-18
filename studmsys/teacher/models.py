221665854548a63229e1e24f67a274231784e640
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-18 12:49:49 +0530

    create new model Teacher

--- studmsys/teacher/models.py
+++ studmsys/teacher/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class Teacher(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50,validators=[
+            RegexValidator(
+                regex=r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&]).{8,50}$',
+                message='Password must contain uppercase, lowercase, number and special character'
+            )
+        ])
+
+    subChoice = [
+        ('M','Marathi'),
+        ('H','Hindi'),
+        ('E','English'),
+        ('S','Science'),
+        ('MA','Mathematics'),
+        ('SC','Social Science'),
+        ('G','Deography'),
+    ]
+    subject = models.CharField(max_length=50, choices= subChoice)
+
+    depChoice =[
+        ('MA','Math'),
+        ('S','Science'),
+        ('M','Marathi'),
+        ('E','English'),
+        ('M','Hindi'),
+
+    ]
+    department = models.CharField(choices=depChoice, max_length=50)
+
+    age = models.IntegerField()
+    address = models.TextField()
+    
