42b6fa07f0726db438ec3df0ce1daf843b72ce88
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-22 18:46:08 +0530

    create function for validate the age is 18 or not

--- studmsys/teacher/forms.py
+++ studmsys/teacher/forms.py
@@ -1,11 +1,23 @@
 from django import forms
-from .models import *
+from .models import Teacher
 
-class TeacherForm(forms.Form):
+
+class TeacherLogin(forms.Form):
     class Meta:
         model = Teacher
         
         fields = '__all__'
         
-    # def cleanAge(self):
-    #     age = 
+    def clean_age(self):
+        age = self.cleaned_data.get('age')
+        if age > 18:
+            raise forms.ValidationError("Age must be 18 or greater than 18")
+        
+        return age
+    
+    
+# class StudentList(forms.Form):
+#     class Meta:
+#         model = 
+
+
