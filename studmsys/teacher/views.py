a0cb86caf1960ddb1ef38f495330cc14765d8dd9
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-25 17:48:47 +0530

    create three logic functions for teacher login, student list, manage subject

--- studmsys/teacher/views.py
+++ studmsys/teacher/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import *
+from .forms import *
 
-# Create your views here.
+def teacherLogin(request):
+    if request == 'POST':
+        form = TeacherLogin(request.post)
+        if form.is_valid():
+            form.save()
+            return render(request, 'temp/dashboard.html')
+    else:
+        form = TeacherLogin()
+        
+    return render(request, 'temp/login.html', {'form': form})
+
+
+def studentList(request):
+    student = Student.objects.all()
+    return render(request, 'temp/studentList.html', {'stud': student})
+
+def manageSubject(request):
+    subject = Teacher.objects.values('subject')
+    return render(request, 'temp/subject.html', {'sub': subject})
